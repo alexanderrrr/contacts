887bda7ff26009623509a599bc73ed5e8373a823
Aleksandar Nojic <acons1987@gmail.com>
2019-12-11 16:14:11 +0100

    Reduced number of users to seed from 10k to 100.

--- database/seeds/ContactsTableSeeder.php
+++ database/seeds/ContactsTableSeeder.php
@@ -12,6 +12,6 @@ class ContactsTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(Contact::class, 10000)->create();
+        factory(Contact::class, 100)->create();
     }
 }
